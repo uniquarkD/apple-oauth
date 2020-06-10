0a0b2fe8073871f13a317f4427c7e2a1eb1cecae
Renan Castro <renanccastro@gmail.com>
2020-06-09 22:57:18 -0300

    transfer ownership

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'renanccastro:apple-oauth',
+  name: 'quave:apple-oauth',
   version: '1.0.2',
   summary: 'Sign in with Apple OAuth flow - fork from bigowl',
-  git: 'https://github.com/renanccastro/apple-oauth',
+  git: 'https://github.com/quavedev/apple-oauth',
 });
 
 Package.onUse(function(api) {
