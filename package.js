bd0288d5480c78c5cd6c1c2adc78afbb6988e513
Renan Castro <renanccastro@gmail.com>
2020-08-14 12:03:57 -0300

    Publishing new patch version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'quave:apple-oauth',
-  version: '1.3.3',
+  version: '1.3.4',
   summary: 'Sign in with Apple OAuth flow - fork from bigowl',
   git: 'https://github.com/quavedev/apple-oauth',
 });
